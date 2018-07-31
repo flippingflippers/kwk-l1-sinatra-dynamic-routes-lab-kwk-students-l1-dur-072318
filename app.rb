cef719bae3e08d2192ef2585038324b9ec49795e
flippingflippers <citrussunshine2004@gmail.com>
2018-07-31 19:52:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
-get '/reversename/:name' do
-  @name=params[:name]
-  @name.reverse
-end
+# get '/reversename/:name' do
+#   @name=params[:name]
+#   @name.reverse
+# end
 
-get '/square/:number'do
-@number=params[:number]
-"#{@number.to_i * @number.to_i}"
-end
+# get '/square/:number'do
+# @number=params[:number]
+# "#{@number.to_i * @number.to_i}"
+# end
 
 get '/say/:number/:phrase' do
 @number=params[:number]
