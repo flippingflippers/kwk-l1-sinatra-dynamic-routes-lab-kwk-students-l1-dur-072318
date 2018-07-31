2914389e92fdb170bf2b8f24a5ac5ac41637b6a4
flippingflippers <citrussunshine2004@gmail.com>
2018-07-31 19:33:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,10 +6,11 @@ class App < Sinatra::Base
 get '/reversename/:name' do
   @name=params[:name]
   @name.reverse
-end
 
 get '/square/:number'do
 @number=params[:number]
 
-get ''
+get '/say/:number/:phrase'
+
+
 end
