b7a7d92b1f8e9b62e555aab22933cae2be116643
flippingflippers <citrussunshine2004@gmail.com>
2018-07-31 19:37:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,20 +13,22 @@ get '/square/:number'do
 
 end
 
-get '/say/:number/:phrase'
-@number=params[:number]
-@phrase=params[:phrase]
-end
+# get '/say/:number/:phrase'
+# @number=params[:number]
+# @phrase=params[:phrase]
+# end
 
-get '/say/:word1/:word2/:word3/:word4/:word5'
-@word1=params[:word1]
-@word2=params
-@word3=params
-@word4=params
-@word5=params
-end
+# get '/say/:word1/:word2/:word3/:word4/:word5'
+# @word1=params[:word1]
+# @word2=params[:word2]
+# @word3=params[:word3]
+# @word4=params[:word4]
+# @word5=params[:word5]
+# end
 
-get '/:operation/:number1/:number2'
-
-end
+# get '/:operation/:number1/:number2'
+# @operation=params[:operation]
+# @number1=params[:number1]
+# @number2=params[:number2]
+# end
 end
